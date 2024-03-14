08883b5a1c0d895034d6e91c1266610d5bacb0c9
Múcio Ferreira <mucio.ferreira@outlook.com>
2024-03-13 23:20:16 -0300

    Adicionar issue controller

---
+++ app/Http/Controllers/IssueController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Issue;
+use Illuminate\Http\Request;
+
+class IssueController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Issue $issue)
+    {
+        //
+    }
+
+    public function edit(Issue $issue)
+    {
+        //
+    }
+
+    public function update(Request $request, Issue $issue)
+    {
+        //
+    }
+
+    public function destroy(Issue $issue)
+    {
+        //
+    }
+}
