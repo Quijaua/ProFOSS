76f8ec9a595f8166b1affadcd0897be68ea466ed
Múcio Ferreira <mucio.ferreira@outlook.com>
2024-03-15 20:47:24 -0300

    Adicionar pacote votable

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Overtrue\LaravelVote\Traits\Votable;
 
 class Issue extends Model
 {
     use HasFactory;
+    use Votable;
 
     protected $fillable = [
         'title',
