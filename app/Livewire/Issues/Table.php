ca4972fae7c4c09955728c24d87abd0fa7f787cc
Múcio Ferreira <mucio.ferreira@outlook.com>
2024-03-16 13:34:25 -0300

    Adicionar tela de perfil

--- app/Livewire/Issues/Table.php
+++ app/Livewire/Issues/Table.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\Issues;
 
 use App\Models\Issue;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -15,9 +17,14 @@ class Table extends Component
         'issue.table.reload' => '$refresh'
     ];
 
+    public ?User $creator = null;
+
     public function render(): View
     {
         $issues = Issue::query()
+            ->when($this->creator, function (Builder $query, User $user): Builder {
+                return $query->whereHas('createdBy', fn(Builder $query) => $query->where('id', $user->id));
+            })
             ->withCount('upvoters')
             ->orderBy('upvoters_count', 'desc')
             ->paginate(25);
